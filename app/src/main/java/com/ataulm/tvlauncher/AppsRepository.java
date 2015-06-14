2e86356b12f5fae2b7ef9d11a2c527a6df6cdfda
Ataul Munim <timberwo7ves@gmail.com>
2015-06-14 18:35:29 +0100

    Fix missing leanback apps

--- app/src/main/java/com/ataulm/tvlauncher/AppsRepository.java
+++ app/src/main/java/com/ataulm/tvlauncher/AppsRepository.java
@@ -7,9 +7,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.util.Log;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -17,6 +15,7 @@ import java.util.Set;
 
 class AppsRepository {
 
+    private static final String CATEGORY_LEANBACK_SETTINGS = "android.intent.category.LEANBACK_SETTINGS";
     private final PackageManager packageManager;
 
     AppsRepository(PackageManager packageManager) {
@@ -30,7 +29,7 @@ class AppsRepository {
     private static Collection<App> getAppsFrom(PackageManager packageManager) {
         Set<App> launchableInstalledApps = new HashSet<>();
         for (ResolveInfo resolveInfo : getInstalledAppsFrom(packageManager)) {
-            App app = obtainAppFrom(packageManager, resolveInfo.activityInfo.applicationInfo);
+            App app = obtainAppFrom(packageManager, resolveInfo);
             if (app.isReal()) {
                 launchableInstalledApps.add(app);
             }
@@ -43,36 +42,65 @@ class AppsRepository {
         activities.addAll(launcherActivitiesIn(packageManager));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             activities.addAll(leanbackActivitiesIn(packageManager));
+            activities.addAll(leanbackSettingsActivitiesIn(packageManager));
         }
         return activities;
     }
 
     private static List<ResolveInfo> launcherActivitiesIn(PackageManager packageManager) {
-        Intent intent = new Intent()
-                .setAction(Intent.ACTION_MAIN)
+        Intent intent = new Intent(Intent.ACTION_MAIN)
                 .addCategory(Intent.CATEGORY_LAUNCHER);
         return getResolveInfos(packageManager, intent);
     }
 
     private static List<ResolveInfo> getResolveInfos(PackageManager packageManager, Intent intent) {
-        return packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return packageManager.queryIntentActivities(intent, 0);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private static List<ResolveInfo> leanbackActivitiesIn(PackageManager packageManager) {
-        Intent intent = new Intent()
-                .setAction(Intent.ACTION_MAIN)
+        Intent intent = new Intent(Intent.ACTION_MAIN)
                 .addCategory(Intent.CATEGORY_LEANBACK_LAUNCHER);
         return getResolveInfos(packageManager, intent);
     }
 
-    private static App obtainAppFrom(PackageManager packageManager, ApplicationInfo applicationInfo) {
-        App.PackageName packageName = new App.PackageName(applicationInfo.packageName);
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static List<ResolveInfo> leanbackSettingsActivitiesIn(PackageManager packageManager) {
+        Intent intent = new Intent(Intent.ACTION_MAIN)
+                .addCategory(CATEGORY_LEANBACK_SETTINGS);
+        return getResolveInfos(packageManager, intent);
+    }
+
+    private static App obtainAppFrom(PackageManager packageManager, ResolveInfo resolveInfo) {
+        ApplicationInfo applicationInfo = resolveInfo.activityInfo.applicationInfo;
+        App.PackageName packageName = resolvePackageName(resolveInfo);
+
         String name = String.valueOf(packageManager.getApplicationLabel(applicationInfo));
         Drawable icon = packageManager.getApplicationIcon(applicationInfo);
-        Intent launchIntent = packageManager.getLaunchIntentForPackage(applicationInfo.packageName);
+        Intent launchIntent = launchIntent(packageManager, packageName.toString());
 
         return createApp(packageName, name, icon, launchIntent);
+    }
+
+    private static App.PackageName resolvePackageName(ResolveInfo resolveInfo) {
+        String packageName = resolveInfo.activityInfo.packageName;
+        if (packageName == null) {
+            packageName = resolveInfo.resolvePackageName;
+        }
+        return new App.PackageName(packageName);
+    }
+
+    private static Intent launchIntent(PackageManager packageManager, String packageName) {
+        Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+        if (intent == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            intent = leanbackLaunchIntent(packageManager, packageName);
+        }
+        return intent;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static Intent leanbackLaunchIntent(PackageManager packageManager, String packageName) {
+        return packageManager.getLeanbackLaunchIntentForPackage(packageName);
     }
 
     private static App createApp(App.PackageName packageName, String appName, Drawable icon, Intent intent) {
